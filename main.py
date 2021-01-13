d5c52acfca0f434eb4d79deec1450f3ade1aac57
burhansaydin <burhasaydin@gmail.com>
2021-01-13 22:15:19 +0200

    -client id and client secret are written in order to access spotify to create a playlist -by using these information authenticated my Pycharm Project -authenticated user id is taken -BeautifulSoup is used to list all songs from the date requested from user -created new list that includes top 100 songs in the specified date

--- main.py
+++ main.py
@@ -3,35 +3,54 @@ import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-user_id = "burhansaydin@gmail.com"
 SPOTIPY_CLIENT_ID = "fe6a678914a343ba9b648e2d9cc1edd7"
 SPOTIPY_CLIENT_SECRET = "64e9deb02d4047c6aabc78001ffc41d6"
-url = "https://www.billboard.com/charts/hot-100/2000-08-12"
-REDIRECT_URI =f"https://api.spotify.com/v1/users/{user_id}/playlists"
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+url = f"https://www.billboard.com/charts/hot-100/{date}"
 
-header = {
-    "Authorization": "playlist-modify-public",
-    "Content-Type" : "application/json",
-    "name" : "new"
-
-}
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id = SPOTIPY_CLIENT_ID,
-                                               client_secret=SPOTIPY_CLIENT_SECRET,
-                                               redirect_uri=REDIRECT_URI,
-                                               header=header))
-
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+            scope="playlist-modify-private",
+            redirect_uri="https://developer.spotify.com/dashboard/applications/fe6a678914a343ba9b648e2d9cc1edd7",
+            client_id=SPOTIPY_CLIENT_ID,
+            client_secret=SPOTIPY_CLIENT_SECRET,
+            show_dialog=True,
+            cache_path="token.txt")
+                     )
+user_id = sp.current_user()["id"]
 
 response = requests.get(url).text
 
 soup = BeautifulSoup(response, "html.parser")
-song = soup.find_all("span", class_="chart-element__information__song text--truncate color--primary")
+songs = soup.find_all("span", class_="chart-element__information__song text--truncate color--primary")
 
-songs = [song.getText() for song in song]
+song_names = [song.getText() for song in songs]
 
-print(songs)
 
-#entry = input("Which year do you want to travel to? Type the date in this format. YYYY-MM-DD\n")
+
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+playlist_id = f"{date}Billboard 100"
+create_playlist = sp.user_playlist_create(user_id, name=playlist_id, public=False)
+new_playlist = sp.user_playlist_add_tracks(user= user_id, playlist_id=create_playlist["id"],tracks=song_uris)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
